381c82916e9f505268ff3d40d8099b90eb839a7a
scotty974 <fabienetheve@hotmail.com>
2024-04-30 10:22:42 +0200

    add logic to draw grid

--- app/src/gamelife/gameLife.tsx
+++ app/src/gamelife/gameLife.tsx
@@ -1,8 +1,15 @@
 import { useRef } from "react";
 function GameLife() {
-  const canvas = useRef<HTMLCanvasElement>(null);
-
-  return <canvas ref={canvas} className="min-h-screen w-full bg-black"></canvas>;
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const drawGrid = () => {
+    const canvas: any = canvasRef.current;
+    if (!canvas.current) return;
+    const ctx = canvas.getContext("2d");
+    
+  };
+  return (
+    <canvas ref={canvasRef} className="min-h-screen w-full bg-black"></canvas>
+  );
 }
 
 export default GameLife;
